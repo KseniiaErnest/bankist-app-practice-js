08860169e0250b32b34c1e2e9e9681b39dc25781
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-12-15 09:18:59 -0500

    Initial commit

---
+++ scriptpractice.js
@@ -0,0 +1,71 @@
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));
+console.log(arr.slice(-1));
+console.log(arr.slice());
+
+// console.log(arr.splice(2));
+
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+
+const letters = arr.concat(arr2);
+console.log(letters);
+
+console.log(letters.join(' - '));
+*/
+/*
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+*/
+
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+movements.forEach(function(movement, index, array){
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+})
+*/
+/*
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function(value, key, mao) {
+console.log(`${key}: ${value}`);
+})
+
+//Set
+const currenciesUnique = new Set(['USD', 'GBR', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach((value, key, map) => {
+console.log(`${key}: ${value}`);
+})
+*/
